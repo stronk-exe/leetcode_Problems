649e2e5ed010c7da45a6150c7b8014972ce9cac4
Stronk_ <94312066+stronk-exe@users.noreply.github.com>
2022-10-23 09:38:10 +0100

    Integer to Roman

---
+++ IntegertoRoman/IntegertoRoman.py
@@ -0,0 +1,24 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        dictio = {
+            1 : 'I',
+            4 : 'IV',
+            5 : 'V',
+            9 : 'IX',
+            10 : 'X',
+            40 : 'XL',
+            50 : 'L',
+            90 : 'XC',
+            100 : 'C',
+            400 : 'CD',
+            500 : 'D',
+            900 : 'CM',
+            1000 : 'M'
+        }
+        res = ""
+        for i in [1000,900,500,400,100,90,50,40,10,9,5,4,1]:
+            while i<=num:
+                res += dictio[i]
+                num-=i
+        return res
+
