1d934e335ae4779348bd4501858479182bbe7720
Stronk_ <94312066+stronk-exe@users.noreply.github.com>
2022-10-31 14:51:03 +0100

    Toeplitz Matrix

--- ToeplitzMatrix/ToeplitzMatrix.py
+++ ToeplitzMatrix/ToeplitzMatrix.py
@@ -1,7 +1,6 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
         for i in range(0,len(matrix)-1):
-            print('>>',matrix[i][0:len(matrix[i])-1],'>>',matrix[i+1][1:len(matrix[i])])
             if matrix[i][0:len(matrix[i])-1] != matrix[i+1][1:len(matrix[i])]:
                 return False
         return True
